f226c619ae61694fdff88258eb4e1fd59886e191
afontanr <afontanr@gmail.com>
2025-03-12 20:09:44 +0100

    initial commit

---
+++ dynamic-pay-gateway/src/main/java/com/afontanr/dynamicpaygateway/DynamicPayGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.afontanr.dynamicpaygateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicPayGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DynamicPayGatewayApplication.class, args);
+    }
+
+}
---
+++ dynamic-pay-gateway/src/test/java/com/afontanr/dynamicpaygateway/DynamicPayGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.afontanr.dynamicpaygateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicPayGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
